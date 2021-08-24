98a27d1f0649db0e617d52a35926f4fe4de3cefa
telf <01.vladimir.mishakov@gmail.com>
2021-08-25 01:48:57 +0300

    Add missing sql.

--- sql/callback_messages.sql
+++ sql/callback_messages.sql
@@ -1,7 +1,10 @@
+-- auto-generated definition
 create table callback_messages
 (
-    message_id int    null,
-    chat_id    bigint null,
-    day        int    null,
-    month      int    null
+    message_id int          null,
+    chat_id    int          null,
+    day        int          null,
+    month      int          null,
+    teacher    varchar(255) null
 );
+
